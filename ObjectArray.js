1ebb72614154946585151990bb00dd5eb8d62ec8
Your Name <you@example.com>
2023-05-01 06:58:58 -0400

    added hook function

---
+++ ObjectArray.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+function ObjectArray() {
+  let [car, setCar] = useState({
+    brand: "Ford",
+    model: "Musting",
+    year: 2020,
+    color: "green",
+  });
+
+  const changeColor = () => {
+    setCar((prevState) => {
+      return { ...prevState, color: "blue" };
+    });
+  };
+
+  return (
+    <div>
+      <h2>
+        {" "}
+        Updating a single property color {car.color} without overwritting the
+        entire state
+      </h2>
+      <p>
+        We'll do by destructing the previous state and then just update the
+        color
+      </p>
+      <button type="button" onClick={changeColor}>
+        {" "}
+        blue
+      </button>
+    </div>
+  );
+}
+export default ObjectArray;
